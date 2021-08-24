4bccbd96ccb6d0f15891b4ce28f6abb26ecd42ec
Joe Sustaric <joe8307@gmail.com>
2021-08-24 17:30:03 +1000

    add in rack protection

--- lib/simple_api/main.rb
+++ lib/simple_api/main.rb
@@ -2,6 +2,10 @@
 
 $LOAD_PATH << File.expand_path(".", __dir__)
 
+require "rack/protection"
 require "simple_api"
+
+# http://sinatrarb.com/protection/
+use Rack::Protection
 
 SimpleApi::Server.run!
